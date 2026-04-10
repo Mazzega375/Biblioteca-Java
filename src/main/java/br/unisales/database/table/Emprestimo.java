be97a2c5522ec5affed86a488037a2bd2188d7ad
Luiz <lamasluizmiguel@gmail.com>
2026-04-10 14:27:42 -0300

    Classe Emprestimo

---
+++ src/main/java/br/unisales/database/table/Emprestimo.java
@@ -0,0 +1,56 @@
+package br.unisales.database.table;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Builder.Default;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "emprestimo")
+public class Emprestimo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "livro_isbn", nullable = false)
+    private Livro livro;
+
+    @Column(name = "data_emprestimo", nullable = false)
+    private LocalDate dataEmprestimo;
+
+    @Column(name = "data_devolucao_prevista", nullable = false)
+    private LocalDate dataDevolucaoPrevista;
+
+    @Column(name = "data_devolucao_real")
+    private LocalDate dataDevolucaoReal;
+
+    @Default
+    @Column(name = "devolvido", nullable = false)
+    private Boolean devolvido = Boolean.FALSE;
+
+    public void devolver(LocalDate dataDevolucaoReal) {
+        this.dataDevolucaoReal = dataDevolucaoReal;
+        this.devolvido = Boolean.TRUE;
+    }
+}
